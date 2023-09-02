b47d626424b4a0a41b5ceba8ba7b4a62a7a0fde1
Hugo Joncour <hugo.joncour.k2o@gmail.com>
2023-09-02 19:03:16 -0400

    fix (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{fs::File, io::Read};
+
 use clap::{App, Arg};
+use torrent::open_torrent;
 
 fn main() {
     let matches = App::new("JSONIFY TORRNET FILE")
@@ -8,5 +11,24 @@ fn main() {
     .arg(Arg::with_name("path"))
     .get_matches();
 
-println!("{}: ", &matches.value_of("path").unwrap());
+    let file_path = matches.value_of("path").unwrap();
+
+    let mut file = match File::open(file_path) {
+      Ok(file) => file,
+      Err(err) => {
+          eprintln!("Error opening file: {}", err);
+          return;
+      }
+  };
+
+  let mut file_contents = Vec::new();
+  if let Err(err) = file.read_to_end(&mut file_contents) {
+      eprintln!("Error reading file: {}", err);
+      return;
+  }
+
+  let torrent = open_torrent(&file_contents);
+
+  println!("{:#?}", torrent);
+
 }
