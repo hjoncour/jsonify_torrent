884f2c9b3283f1064e3888555efab4e76032729e
Hugo Joncour <hugo.joncour.k2o@gmail.com>
2023-09-04 17:54:27 -0400

    fix (#3)

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,35 @@ use std::{fs::File, io::Read};
 
 use clap::{App, Arg};
 use torrent::open_torrent;
+use torrent::encode::save_to_json_file;
 
 fn main() {
-    let matches = App::new("JSONIFY TORRNET FILE")
-    .version("0.1")
-    .author("@hjoncour")
-    .about("Small cli app to convert .torrent to .json")
-    .arg(Arg::with_name("path"))
+    let matches = App::new("JSONIFY TORRENT FILE")
+        .version("0.1")
+        .author("@hjoncour")
+        .about("Small cli app to convert .torrent to .json")
+        .arg(Arg::with_name("input_path")
+        .required(true)
+        .takes_value(true)
+        .index(1)
+        .help("Input path for the .torrent file"))
+    .arg(Arg::with_name("output_path")
+        .required(false)
+        .takes_value(true)
+        .index(2)
+        .help("Output path for the file"))
+    .arg(Arg::with_name("output_type")
+        .required(false)
+        .takes_value(true)
+        .index(3)
+        .help("Type of the output"))
     .get_matches();
 
-    let file_path = matches.value_of("path").unwrap();
+    let input_path = matches.value_of("input_path").unwrap();
+    let output_path: Option<&str> = matches.value_of("output_path");
+    let output_type = matches.value_of("output_type");
 
-    let mut file = match File::open(file_path) {
+    let mut file = match File::open(input_path) {
       Ok(file) => file,
       Err(err) => {
           eprintln!("Error opening file: {}", err);
@@ -28,7 +45,8 @@ fn main() {
   }
 
   let torrent = open_torrent(&file_contents);
-
-  println!("{:#?}", torrent);
+  let name = save_to_json_file(torrent.unwrap(), output_path, output_type);
+  println!("here0");
+  println!("name: {}", name.unwrap());
 
 }
